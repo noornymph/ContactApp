629d88f48da818e8235f368b2562fd5f12da3a1c
noornymph <126329291+noornymph@users.noreply.github.com>
2023-08-10 20:34:55 +0500

    1st commit

---
+++ Firstprog.py
@@ -0,0 +1,28 @@
+def main():
+    startup()
+    step1()
+    step2()
+    a=12,b=67.7
+    print(a+b)
+
+
+def startup():
+    print('Welcome to the Menu\n')
+    print('This program tells ypu how to disassemble an ACM machine\n')
+    print('There are four steps in total\n')
+    print('Press Enter to proceed\n')
+    print()
+
+
+def step1():
+    print('This is step 1\n')
+    print('press enter to proceed\n')
+    print()
+
+
+def step2():
+    print('This is step2\n')
+    print('Press enter to proceed\n')
+    print()
+
+main()
---
+++ main.py
@@ -0,0 +1,24 @@
+
+def startup():
+    print('Welcome to the Menu\n')
+    print('This program tells you how to disassemble an ACM machine\n')
+    print('There are four steps in total\n')
+    print('Press Enter to proceed\n')
+
+
+def step1():
+    print('This is step 1\n')
+    print('press enter to proceed\n')
+
+
+def step2():
+    print('This is step2\n')
+    print('Press enter to proceed\n')
+
+def main():
+    startup()
+    step1()
+    step2()
+
+main()
+
